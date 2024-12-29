4e01dbbf52fdcab8952bb8f619e1be3ca8b5dc1b
awesomemin <saogig0503@naver.com>
2024-12-29 23:25:35 +0900

    히스토리 디자인 개선

--- src/app/history.tsx
+++ src/app/history.tsx
@@ -4,6 +4,9 @@ import HistoryItem from './historyItem';
 export default function History({ history }: { history: Streaming[] }) {
   return (
     <div className="flex flex-col mx-5 mt-4">
+      {history.length !== 0 && (
+        <h2 className="text-center font-bold tracking-widest">히스토리</h2>
+      )}
       {history.map((streaming) => (
         <HistoryItem key={streaming.channelId} streaming={streaming} />
       ))}
--- src/app/historyItem.tsx
+++ src/app/historyItem.tsx
@@ -7,9 +7,11 @@ export default function HistoryItem({ streaming }: { streaming: Streaming }) {
       <a
         href={`https://chzzk.naver.com/live/${streaming.channelId}`}
         target="_blank"
-        className="py-2 border-b border-[#333333]"
+        className="py-3 border-b border-[#333333]"
       >
-        <div className="flex-grow break-words mb-1">{streaming.liveTitle}</div>
+        <div className="flex-grow break-words mb-1 text-sm">
+          {streaming.liveTitle}
+        </div>
         <div className="flex flex-shrink-0 items-center gap-1">
           <Image
             src={
@@ -19,9 +21,9 @@ export default function HistoryItem({ streaming }: { streaming: Streaming }) {
             width={120}
             height={120}
             alt="cha"
-            className="w-6 h-6 rounded-full"
+            className="w-4 h-4 rounded-full"
           />
-          <div className="text-[#777777] font-bold">
+          <div className="text-[#777777] text-sm font-medium">
             {streaming.channelName}
           </div>
         </div>
